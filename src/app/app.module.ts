a01fcf3e194db73eb222ab8057183150aa42e6e3
amira <amira.a.a.hashim@gmail.com>
2020-06-01 07:57:38 +0200

    header attempt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HeaderComponent } from './views/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // HeaderComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/views/posts/posts.module.ts
+++ src/app/views/posts/posts.module.ts
@@ -5,10 +5,11 @@ import { PostsRoutingModule } from './posts-routing.module';
 import { PostsListComponent } from './posts-list/posts-list.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { EditPostComponent } from './edit-post/edit-post.component';
+import { HeaderComponent } from '../header/header.component';
 
 
 @NgModule({
-  declarations: [PostsListComponent, AddPostComponent, EditPostComponent],
+  declarations: [PostsListComponent, AddPostComponent, EditPostComponent, HeaderComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
