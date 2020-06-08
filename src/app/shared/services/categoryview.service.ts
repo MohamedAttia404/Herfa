0cb8dbe357f732023c08578ed5cd10f34cffec76
attia <mohamedahmed4362gmail.com>
2020-06-08 23:09:38 +0200

    ctegoryview

--- src/app/shared/services/categoryview.service.ts
+++ src/app/shared/services/categoryview.service.ts
@@ -7,32 +7,24 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CourseviewService {
+export class CategoryviewService {
 
   prev: string="";
   next: string ="";
 
   constructor(private http: HttpClient) { }
 
+
   parseLinks(links){
     this.prev=links.prev;
     this.next=links.next;
-  }  
-
-  // search function
-  search(data){
-    return this.http.get(`${ environment.apiUrl }/api/search/{data}`);
-  }
-
+  } 
 
   // get all courses
   public getAll(){
     console.log("getAll");
 
-    return this.http.get(`${ environment.apiUrl }/api/courses`, {  params: new HttpParams({fromString: "_page=1&_limit=20"}), observe: "response"}).pipe(retry(3), tap((res: any) => {
-      console.log(res.body.data);
-      this.parseLinks(res.body.links);
-    }));
+    return this.http.get(`${ environment.apiUrl }/api/categories`);
     // return this.http.get(this._courseApi);
   }
 
@@ -47,23 +39,24 @@ export class CourseviewService {
 
   // Delete Course
   delete(id){
-    return this.http.delete(`${ environment.apiUrl }/api/courses/${id}`);
+    return this.http.delete(`${ environment.apiUrl }/api/categories/${id}`);
   }
 
   // Add new course
   add(data){
-    return this.http.post(`${ environment.apiUrl }/api/courses`,data);
+    return this.http.post(`${ environment.apiUrl }/api/categories`,data);
   }
 
   //Get course
   getCourse(id){
-    return this.http.get(`${ environment.apiUrl }/api/courses/${id}`);
+    return this.http.get(`${ environment.apiUrl }/api/categories/${id}`);
   }
 
   // Edit Courses
   update(data, id){
-    return this.http.put(`${ environment.apiUrl }/api/courses/${id}`,data);
+    return this.http.put(`${ environment.apiUrl }/api/categories/${id}`,data);
   }
 
-}
 
+
+}
