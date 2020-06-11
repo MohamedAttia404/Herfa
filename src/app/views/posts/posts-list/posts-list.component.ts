6b97ba5de013dd04fe6813c434e64d5e7e0f09ef
Tarek-Hassan <tarek446240@gmail.com>
2020-06-11 20:01:08 +0200

    edit Permission

--- src/app/views/posts/posts-list/posts-list.component.ts
+++ src/app/views/posts/posts-list/posts-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/shared/services/posts.service';
 import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/shared/services/user.service';
 
 
 @Component({
@@ -11,79 +12,83 @@ import { ToastrService } from 'ngx-toastr';
 export class PostsListComponent implements OnInit {
 
   posts: any=[];
- 
-  constructor(private _postsService: PostsService,
-    private toastr: ToastrService) { }
-//===============================================================================
-  getPosts(){
-    console.log("get posts Component");
-   
-    this._postsService.getPosts().subscribe((res: any) =>{
-     console.log(res.body.links);
-    this.posts = res.body.data;
-    });
-  }
-//===============================================================================
-
-public firstPage() {
-  this.posts = [];
-  this._postsService.sendGetRequestToUrl(this._postsService.first).subscribe((res: any) => {
-    console.log(res);
-    this.posts = res.body.data;
-  })
-}
-
-// //===============================================================
-
-public previousPage() {
-
-  if (this._postsService.prev !== undefined && this._postsService.prev !== null) {
-    this.posts = [];
-    this._postsService.sendGetRequestToUrl(this._postsService.prev).subscribe((res:any) => {
-      console.log(res);
-      this.posts = res.body.data;
-    })
-  }
-
-}
-// //=============================================================================
-
-public nextPage() {
-  if (this._postsService.next !== undefined && this._postsService.next !== null) {
-    this.posts = [];
-    this._postsService.sendGetRequestToUrl(this._postsService.next).subscribe((res:any) => {
-      console.log(res);
-      this.posts = res.body.data;
-    })
-  }
-}
-
-// //====================================================================================
-
-public lastPage() {
-  this.posts = [];
-  this._postsService.sendGetRequestToUrl(this._postsService.last).subscribe((res: any) => {
-    console.log(res);
-    this.posts = res.body.data;
-  })
-}
-//================================================================================
-deleteItem(id:number){
-  console.log("delete");
+  auth:any;
   
-    this._postsService.deletePost(id).subscribe((res:any )=> {
-      console.log("hello");
+  constructor(private _postsService: PostsService,
+    private userService:UserService,
+    private toastr: ToastrService) { }
+    //===============================================================================
+    getPosts(){
+      console.log("get posts Component");
       
-      this.toastr.success('post deleted successfuly', 'success', {timeOut:1000, closeButton: true, progressBar: true});
-      console.log("delete res: "+res);
+      this._postsService.getPosts().subscribe((res: any) =>{
+        console.log(res.body.links);
+        this.posts = res.body.data;
+      });
+    }
+    //===============================================================================
+    
+    public firstPage() {
+      this.posts = [];
+      this._postsService.sendGetRequestToUrl(this._postsService.first).subscribe((res: any) => {
+        console.log(res);
+        this.posts = res.body.data;
+      })
+    }
+    
+    // //===============================================================
+    
+    public previousPage() {
       
+      if (this._postsService.prev !== undefined && this._postsService.prev !== null) {
+        this.posts = [];
+        this._postsService.sendGetRequestToUrl(this._postsService.prev).subscribe((res:any) => {
+          console.log(res);
+          this.posts = res.body.data;
+        })
+      }
+      
+    }
+    // //=============================================================================
+    
+    public nextPage() {
+      if (this._postsService.next !== undefined && this._postsService.next !== null) {
+        this.posts = [];
+        this._postsService.sendGetRequestToUrl(this._postsService.next).subscribe((res:any) => {
+          console.log(res);
+          this.posts = res.body.data;
+        })
+      }
+    }
+    
+    // //====================================================================================
+    
+    public lastPage() {
+      this.posts = [];
+      this._postsService.sendGetRequestToUrl(this._postsService.last).subscribe((res: any) => {
+        console.log(res);
+        this.posts = res.body.data;
+      })
+    }
+    //================================================================================
+    deleteItem(id:number){
+      console.log("delete");
+      
+      this._postsService.deletePost(id).subscribe((res:any )=> {
+        console.log("hello");
+        
+        this.toastr.success('post deleted successfuly', 'success', {timeOut:1000, closeButton: true, progressBar: true});
+        console.log("delete res: "+res);
+        
+        this.getPosts();
+      });
+    }
+    
+    //================================================================================
+    ngOnInit(): void {
+      this.auth=this.userService.isAuthenticated();
       this.getPosts();
-  });
-}
-
-//================================================================================
-  ngOnInit(): void {
-    this.getPosts();
+    }
+    
   }
-
-}
+  
