d8fcd750f71c509e61eeffe564f17168dba10aa8
attia <mohamedahmed4362gmail.com>
2020-06-13 04:01:33 +0200

    course validation and admin style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-gitfimport { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
