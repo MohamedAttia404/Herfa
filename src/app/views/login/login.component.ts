408c325ca0e339db6e8052760af0c5908f4f53cb
Tarek-Hassan <tarek446240@gmail.com>
2020-06-16 18:01:41 +0200

    Vaildation LoginRegister

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -4,6 +4,8 @@ import { loginuser } from 'src/app/models/loginuser.model';
 import { UserService } from 'src/app/shared/services/user.service';
 // import {UserService} from '../users/services/user.service';
 // import {loginuser} from './loginUser.model';
+import { FormGroup, FormBuilder, Validators,FormControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -12,34 +14,41 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class LoginComponent implements OnInit {
   user = new loginuser();
+  uploadForm: FormGroup; 
+  submitted = false;
 
   constructor(
     private userService: UserService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
     private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.uploadForm = this.formBuilder.group(
+      {
+     email:  new FormControl ('',[Validators.required,Validators.email]), 
+     password:  new FormControl ('',[Validators.required,Validators.minLength(6)]),
+    });
+  }
 
   onSubmit() {
+    this.submitted = true;
+    if (this.uploadForm.invalid) {
+      return;
+    }
 
-    const user = {
-      ...this.user
-    };
 
 
 
-    this.userService.singIn(user).subscribe((res: any) => {
-      if (res.success) {
+    this.userService.singIn(this.uploadForm.value).subscribe((res: any) => {
+
         this.userService.setToken(res.data);
         this.router.navigate(['/user/posts']);
+    }, error => {
+      this.toastr.error(error.statusText, 'Email Or Password  May be Not Correct!', {timeOut:3000, closeButton: true, progressBar: true});
       
-      }else{
-          console.log("untrusgh user");
-        this.router.navigate(['/login']);
-
-      }
-
-
-    });
+  });
+    
 
   }
 
